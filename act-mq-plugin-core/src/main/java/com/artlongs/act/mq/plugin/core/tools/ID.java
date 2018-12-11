bf67e81dc1d86d6b0fbd15614819ef14f7ba4818
liqingfeng <liqf@pvc123.com>
2018-12-11 17:29:32 +0800

    fix: ID生成优化

--- act-mq-plugin-core/src/main/java/com/artlongs/act/mq/plugin/core/tools/ID.java
+++ act-mq-plugin-core/src/main/java/com/artlongs/act/mq/plugin/core/tools/ID.java
@@ -1,11 +1,18 @@
 package com.artlongs.act.mq.plugin.core.tools;
 
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.osgl.$;
+import org.osgl.util.S;
+
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 简单 ID 生成
- * 长度 20 位以上 = "yyyyMMddHHmmssSSS"(17) + 3顺序数累加
+ * 长度 19 位 = "yyyyMMddHHmmssSSS"(17) + (2位)原子顺序数累加
  * Created by ${leeton} on 2018/11/21.
  */
 public enum ID {
@@ -16,7 +23,7 @@ public enum ID {
     private long lastTimestamp = 0;
     private long fistNum = 100;
     private static final AtomicInteger atomicNum = new AtomicInteger();
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 
 
     private StringBuffer getYmdId() {
@@ -31,24 +38,23 @@ public enum ID {
         while (currentTimes <= lastTimestamp) {
             ymd=getYmdId();
             currentTimes = Long.valueOf(ymd.append(getNewAutoNum()).toString());
-
         }
 
         //上次生成ID的时间截
         lastTimestamp = currentTimes;
 
-        return currentTimes+"";
+        return String.valueOf(currentTimes);
     }
 
     public String getNewAutoNum(){
         //线程安全的原子操作，所以此方法无需同步
         int newNum = atomicNum.incrementAndGet();
-        if(atomicNum.get()>=999){
+        if(atomicNum.get()>99){
             atomicNum.set(0);
             newNum=atomicNum.get();
         }
-        //数字长度为3位，长度不够数字前面补0
-        String newStrNum = String.format("%03d", newNum);
+        //数字长度为2位，长度不够数字前面补0
+        String newStrNum = String.format("%02d", newNum);
         return newStrNum;
     }
 
@@ -58,8 +64,12 @@ public enum ID {
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 2000; i++) {
-            System.err.println(ID.ONLY.id());
+        for (int i = 0; i < 100; i++) {
+            String idStr =ID.ONLY.id();
+            long id = Long.valueOf(idStr);
+            String ymd = StringUtils.left(idStr,17);
+            System.err.println(id);
+            System.err.println(sdf.format(System.currentTimeMillis()).equals(ymd));
         }
 
     }
